6d7c94f3e0a1d48fb0696ca7aac79baa67599b96
MaciejRydzak <maciej.rydzak@dataedo.com>
2025-02-11 09:07:16 +0100

    init

--- RPS.py
+++ RPS.py
@@ -1,10 +1,26 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import random
+from collections import defaultdict
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+def player(prev_play,
+           opponent_history=[],
+           transition_counts=defaultdict(lambda: defaultdict(int))):
+    
+    if prev_play:
+        opponent_history.append(prev_play)
+    
+    if len(opponent_history) < 4:
+        return random.choice(['R', 'P', 'S'])
+    
+    last_four_moves = tuple(opponent_history[-5:-1])
+    current_move = opponent_history[-1]
+    transition_counts[last_four_moves][current_move] += 1
 
-    return guess
+    last_four_moves = tuple(opponent_history[-4:])
+    if last_four_moves in transition_counts and transition_counts[last_four_moves]:
+        next_move_prediction = max(transition_counts[last_four_moves], key=transition_counts[last_four_moves].get)
+    else:
+        next_move_prediction = random.choice(['R', 'P', 'S'])
+    
+    ideal_response = {'R': 'P', 'P': 'S', 'S': 'R'}
+    return ideal_response[next_move_prediction]
